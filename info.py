54ccb094d04676102d75223744bc02c89292da87
rinoffi <sivaneshoffi@gmail.com>
2025-10-18 07:52:04 +0530

    Update info.py

--- info.py
+++ info.py
@@ -28,14 +28,14 @@ MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+ASBMRfT6V60zYz
 
 # Pics 
 
-QR_CODE = environ.get('QR_CODE', 'https://graph.org/file/ccb9db43e62a2e524928e.jpg')
-START_IMG = environ.get('START_IMG', 'https://i.ibb.co/LdT5fdJY/photo-2025-08-13-01-12-38-7537871916074270724.jpg')
-FSUB_PICS = (environ.get('FSUB_PICS', 'https://graph.org/file/7478ff3eac37f4329c3d8.jpg https://graph.org/file/56b5deb73f3b132e2bb73.jpg')).split() 
+QR_CODE = environ.get('QR_CODE', 'https://files.catbox.moe/o9dc69.jpg')
+START_IMG = environ.get('START_IMG', 'https://files.catbox.moe/qjzvgz.jpg')
+FSUB_PICS = (environ.get('FSUB_PICS', 'https://files.catbox.moe/9u01o5.jpg https://files.catbox.moe/433i75.jpg')).split() 
 
 # File Limit
 
 IS_FILE_LIMIT = is_enabled('IS_FILE_LIMIT', True) # Enable Or Disable File Limit
-FILES_LIMIT = int(environ.get("FREE_FILES", "3")) #No. of File User Gets In Free
+FILES_LIMIT = int(environ.get("FREE_FILES", "1")) #No. of File User Gets In Free
 
 # Database Settings
 
