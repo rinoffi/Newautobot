0e1465114e86380fb04614c2634e79c88add9e2a
rinoffi <sivaneshoffi@gmail.com>
2025-10-18 07:47:12 +0530

    Update info.py

--- info.py
+++ info.py
@@ -18,13 +18,13 @@ def is_valid_ip(ip):
 
 # Main Variables 
 
-API_ID = int(environ.get('API_ID', ''))
-API_HASH = environ.get('API_HASH', '')
+API_ID = int(environ.get('API_ID', '24160099'))
+API_HASH = environ.get('API_HASH', '381515f2540b0bf817a68af8b352f5b7')
 BOT_TOKEN = environ.get('BOT_TOKEN', '')
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
-USERNAME = environ.get('USERNAME', "https://telegram.me/Silicon_Official")
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
-MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/LuciferCinema')
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6248675084').split()]
+USERNAME = environ.get('USERNAME', "https://telegram.me/Thizz_zoro")
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002853893090'))
+MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+ASBMRfT6V60zYzI1')
 
 # Pics 
 
